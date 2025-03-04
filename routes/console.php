3817464f562ac2ba708534dfabac47cac6246835
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-03-04 21:42:58 +0530

    dd

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,28 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('create:user', function () {
+    $email = $this->ask('What is the user email?');
+    $name = $this->ask('What is the user name?');
+    $password = $this->secret('What is the user password?');
+
+    $user = User::create([
+        'email' => $email,
+        'name' => $name,
+        'password' => Hash::make($password),
+        'level' => 1,
+        'prizes' => [],
+        'rank' => [],
+        'is_admin' => false,
+        'last_seen' => now(),
+        'streak' => [],
+        'description' => '',
+        'is_team' => false,
+        'banner' => null,
+        'ban' => false,
+        'avatar' => null
+    ]);
+
+    $this->info("User {$user->name} created successfully!");
+})->purpose('Create a new user');
