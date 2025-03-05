9b43d2dfe8424d3cde642aa9c3e748ce4c19c693
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-03-05 14:08:43 +0530

    Favicon update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="https://i.ibb.co/YFjYM97v/Study-LABS.png" type="image/png">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
