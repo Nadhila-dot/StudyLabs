191475deb94a3400dfced174f53110319abf6a6d
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-03-05 09:11:18 +0530

    test->

--- app/Http/Middleware/admin/Adminonly.php
+++ app/Http/Middleware/admin/Adminonly.php
@@ -5,15 +5,19 @@ namespace App\Http\Middleware\Admin;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use App\Http\Middleware\HandleInertiaRequests;
 
-class AdminOnly
+class AdminOnly extends Inertia
 {
+    protected $rootView = 'app';
+
     public function handle(Request $request, Closure $next): mixed
     {
         if (!Auth::check() || !Auth::user()->is_admin) {
-            return redirect('/404');
+            return Inertia::render('Errors/404');
         }
 
-        return $next($request);
+        return parent::handle($request, $next);
     }
 }
