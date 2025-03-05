86c286ccb6b430db85c6d19c28af1c5970b32682
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-03-05 09:03:11 +0530

    user->AdminOnly.php

--- app/Http/Middleware/admin/Adminonly.php
+++ app/Http/Middleware/admin/Adminonly.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use App\Http\Middleware\HandleInertiaRequests;
+use Inertia\Middleware;
 
 class AdminOnly extends HandleInertiaRequests
 {
